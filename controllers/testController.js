5a1781f32fda122146f8a6d3a622ed2a2e7b8741
Alief Arifin M <aliefarifin99@gmail.com>
2024-12-04 10:51:56 +0700

    deleting cloud-storage modul

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,36 +1,5 @@
 const redisClient = require("../config/redistClient");
-const { Storage } = require("@google-cloud/storage");
 const db = require("../config/db");
-
-const storage = new Storage({
-  projectId: process.env.GCP_PROJECT_ID,
-  keyFilename: process.env.GCP_KEY_FILE,
-});
-const bucketName = process.env.GCP_BUCKET_NAME;
-
-// Test Google Cloud Storage (GCS) Connectivity
-exports.testGCS = async (req, res) => {
-  try {
-    const bucket = storage.bucket(bucketName);
-
-    const [exists] = await bucket.exists();
-
-    if (exists) {
-      res.status(200).json({
-        message: "Google Cloud Storage is connected and bucket exists!",
-      });
-    } else {
-      res.status(404).json({
-        message: "Bucket does not exist!",
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      message: "Failed to connect to Google Cloud Storage!",
-      error: error.message,
-    });
-  }
-};
 
 // Test API Connectivity
 exports.testAPI = (req, res) => {
--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { testAPI, testDB, testRedis, testGCS } = require("../controllers/testController");
+const { testAPI, testDB, testRedis } = require("../controllers/testController");
 
 router.get("/api", testAPI);
 router.get("/db", testDB);
 router.get("/redist", testRedis);
-router.get("/gcs", testGCS)
 
 module.exports = router;
